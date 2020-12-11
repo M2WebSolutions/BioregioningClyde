9bdffe54b72f035d486b9dce32e50aec0ee0a4f7
CairnMacGregor <60977182+CairnMacGregor@users.noreply.github.com>
2020-12-11 14:55:11 +0000

    Initial Commit
    Index and styles with links

---
+++ app.js
@@ -0,0 +1,17 @@
+  
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+const header = document.querySelector('.main-header');
+
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if (scrollPos > 10) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+});
+
